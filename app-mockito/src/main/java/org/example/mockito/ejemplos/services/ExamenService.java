4c4a88a971d1a5d21dba80b12baf607ddbde5e34
Facundo Paolini <fpaolini@technisys.com>
2021-08-03 11:36:44 -0300

    Mockito1stExample

--- app-mockito/src/main/java/org/example/mockito/ejemplos/services/ExamenService.java
+++ app-mockito/src/main/java/org/example/mockito/ejemplos/services/ExamenService.java
@@ -2,6 +2,8 @@ package org.example.mockito.ejemplos.services;
 
 import org.example.mockito.ejemplos.models.Examen;
 
+import java.util.Optional;
+
 public interface ExamenService {
-    Examen findExamenPorNombre(String nombre);
+    Optional<Examen> findExamenPorNombre(String nombre);
 }
--- app-mockito/src/test/java/org/example/mockito/ejemplos/services/ExamenServiceImplTest.java
+++ app-mockito/src/test/java/org/example/mockito/ejemplos/services/ExamenServiceImplTest.java
@@ -2,17 +2,40 @@ package org.example.mockito.ejemplos.services;
 
 import org.example.mockito.ejemplos.models.Examen;
 import org.example.mockito.ejemplos.repositories.ExamenRepository;
-import org.example.mockito.ejemplos.repositories.ExamenRepositoryImpl;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ExamenServiceImplTest {
 
     @Test
     void findExamenPorNombre() {
-        ExamenRepository repository = new ExamenRepositoryImpl();
+        ExamenRepository repository = mock(ExamenRepository.class);
         ExamenService service = new ExamenServiceImpl(repository);
-        Examen examen = service.findExamenPorNombre("Matematicas");
+        List<Examen> datos = Arrays.asList(new Examen(5L,"Matematicas"),new Examen(6L,"Lenguaje"),
+                new Examen(7L,"Historia"));
+        when(repository.findAll()).thenReturn(datos);
+        Optional<Examen> examen = service.findExamenPorNombre("Matematicas");
+
+        assertTrue(examen.isPresent());
+        assertEquals(5L,examen.orElseThrow().getId());
+        assertEquals("Matematicas",examen.get().getNombre());
+    }
+
+    @Test
+    void findExamenPorNombreListaVacia() {
+        ExamenRepository repository = mock(ExamenRepository.class);
+        ExamenService service = new ExamenServiceImpl(repository);
+        List<Examen> datos = Collections.emptyList();
+        when(repository.findAll()).thenReturn(datos);
+        Optional<Examen> examen = service.findExamenPorNombre("Matematicas");
+
+        assertFalse(examen.isPresent());
     }
 }
