f21e9e21fcbe8170d4b304e981dbab7de8bdc04f
Facundo <facundo.paolini@gmail.com>
2021-08-12 00:54:54 -0300

    Test reupload

---
+++ app-mockito/src/test/java/org/example/mockito/ejemplos/services/ExamenServiceImplTest.java
@@ -0,0 +1,135 @@
+package org.example.mockito.ejemplos.services;
+
+import org.example.mockito.ejemplos.models.Examen;
+import org.example.mockito.ejemplos.repositories.ExamenRepository;
+import org.example.mockito.ejemplos.repositories.PreguntaRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+
+@ExtendWith(MockitoExtension.class)
+        //->lo mismo que MockitoAnnotations.openMocks(this) para la injeccion de dep.
+class ExamenServiceImplTest {
+
+    @Mock
+    ExamenRepository repository;
+
+    @Mock
+    PreguntaRepository preguntaRepository;
+
+    @InjectMocks
+    ExamenServiceImpl service;
+
+
+//    @BeforeEach
+//    void setUp() {
+//        //I can use tags for this
+//        repository = mock(ExamenRepository.class);
+//        preguntaRepository = mock(PreguntaRepository.class);
+//        service = new ExamenServiceImpl(repository,preguntaRepository);
+//        MockitoAnnotations.openMocks(this);
+//    }
+
+    @Test
+    void findExamenPorNombre() {
+
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        Optional<Examen> examen = service.findExamenPorNombre("Matematicas");
+
+        assertTrue(examen.isPresent());
+        assertEquals(5L, examen.orElseThrow().getId());
+        assertEquals("Matematicas", examen.get().getNombre());
+    }
+
+    @Test
+    void findExamenPorNombreListaVacia() {
+        List<Examen> datos = Collections.emptyList();
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        Optional<Examen> examen = service.findExamenPorNombre("Matematicas");
+
+        assertTrue(examen.isPresent());
+    }
+
+    @Test
+    void testPreguntasExamen() {
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        when(preguntaRepository.findPreguntaSPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        Examen examen = service.findExamenPorNombreConPreguntas("Matematicas");
+        assertEquals(5, examen.getPreguntas().size());
+        assertTrue(examen.getPreguntas().contains("Aritmetica"));
+    }
+
+    @Test
+    void testPreguntasExamenVerify() {
+        when(repository.findAll()).thenReturn(Datos.EXAMENES);
+        when(preguntaRepository.findPreguntaSPorExamenId(anyLong())).thenReturn(Datos.PREGUNTAS);
+        Examen examen = service.findExamenPorNombreConPreguntas("Matematicas");
+        assertEquals(5, examen.getPreguntas().size());
+        assertTrue(examen.getPreguntas().contains("Aritmetica"));
+        verify(repository).findAll();
+        verify(preguntaRepository).findPreguntaSPorExamenId(anyLong());
+    }
+
+    @Test
+    void testNoExisteExamenVerify() {
+        when(repository.findAll()).thenReturn(Collections.emptyList());
+        Examen examen = service.findExamenPorNombreConPreguntas("Matematicas");
+        assertNull(examen);
+        verify(repository).findAll();
+    }
+
+    @Test
+    void testGuardarExamen() {
+        //GIVEN
+        Examen newExamen = Datos.EXAMEN;
+        newExamen.setPreguntas(Datos.PREGUNTAS);
+        when(repository.guardar(any(Examen.class))).then(new Answer<Examen>() {
+            Long secuencia = 8L;
+
+            @Override
+            public Examen answer(InvocationOnMock invocationOnMock) throws Throwable {
+                Examen examen = invocationOnMock.getArgument(0);
+                examen.setId(secuencia++);
+                return examen;
+            }
+        });
+        // WHEN
+        Examen examen = service.guardar(newExamen);
+
+        //THEN
+        assertNotNull(examen.getId());
+        assertEquals(8L, examen.getId());
+        assertEquals("Fisica", examen.getNombre());
+        verify(repository).guardar(any(Examen.class));
+        verify(preguntaRepository).guardarVarias((anyList()));
+    }
+
+    @Test
+    void testManejoExceptions() {
+        when(repository.findAll()).thenReturn(Datos.EXAMENES_ID_NULL);
+        when(preguntaRepository.findPreguntaSPorExamenId(isNull())).thenThrow(IllegalArgumentException.class);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            service.findExamenPorNombreConPreguntas("Matematicas");
+        });
+        assertEquals(IllegalArgumentException.class,exception.getClass());
+
+        verify(repository).findAll();
+        verify(preguntaRepository).findPreguntaSPorExamenId(isNull());
+    }
+}
